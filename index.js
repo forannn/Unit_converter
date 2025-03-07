ecf30e3efe1bae1a5355d14598e415f71c000933
forannn <forann133@gmail.com>
2025-03-07 21:58:45 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ document.getElementById("convert").addEventListener("click", function() {
 
     document.getElementById("render2").textContent = `${number.value} liters = ${(number.value * 0.264).toFixed(3)} gallons | ${number.value} gallons = ${(3.787 * number.value).toFixed(3)} liters`
 
-    document.getElementById("number").textContent = `${number.value} kilos = ${(number.value * 2.204).toFixed(3)} pounds | ${number.value} pounds = ${(0.453 * number.value).toFixed(3)} kilos`
+    document.getElementById("render3").textContent = `${number.value} kilos = ${(number.value * 2.204).toFixed(3)} pounds | ${number.value} pounds = ${(0.453 * number.value).toFixed(3)} kilos`
 
 })
